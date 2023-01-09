07466a8a0652d2b9b25a27cab823b1459254b06b
sebastiencandau <scallpie@gmail.com>
2023-01-09 10:41:18 +0100

    add route dependencies and prepare project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Views/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
