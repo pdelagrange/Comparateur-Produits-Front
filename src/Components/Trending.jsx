b62a109c05ab25a6dbe87c55546dfd297ba25897
fchabellard <felix.chabellard@etu.u-bordeaux.fr>
2023-01-09 14:08:06 +0100

    bases accueil, recherche, populaires + css

---
+++ src/Components/Trending.jsx
@@ -0,0 +1,19 @@
+// Composant "Populaires"
+const Trending = () => {
+    return (
+        <div id="wrapper">
+            <h2>Populaires</h2>
+            {   
+                    //x.map((x, *index*) => {
+                    <div id="itemPreview">
+                        <img>{/*x.image*/}</img>
+                        <h3>{/*x.name*/}</h3>
+                        <p>{/*x.prix*/}</p>
+                    </div>
+                    //});
+            }    
+        </div>
+    )
+}
+
+export default Trending;
