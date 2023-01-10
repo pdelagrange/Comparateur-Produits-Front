de2374e1af66a36e5a38c4679abd5de454be55a2
augcolas <augus.colas@orange.fr>
2023-01-10 09:25:38 +0100

    creating users services

--- src/Services/User.service.jsx
+++ src/Services/User.service.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
-export const getUser = async (username) => {
+export const getUsers = async () => {
+    return await fetch(`api-url`);
+}
+
+export const getUser = async(username) => {
     return await fetch(`api-url?${username}`);
 }
+
+export const createUser = async (username,password) => {
+    return await fetch(
+        `api-url?${username,password}`,
+        {
+            method:'POST',
+        });
+}
+
+export const deleteUser = async (id) => {
+    return await fetch(
+        `api-url?${id}`,
+        {
+            method:'POST',
+        });
+}
+
