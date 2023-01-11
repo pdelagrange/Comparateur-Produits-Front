72328c6bd612e8a7f6e10aec3f75a08d73bb7d0a
augcolas <augus.colas@orange.fr>
2023-01-11 16:38:02 +0100

    category click, account admin check

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -7,9 +7,11 @@ function Account() {
     const token = useToken();
     const user = token.getUserConnected();
 
+    console.log(user)
     return (
         <div>
             {user && <p className='fs-4 text-white mx-4'>Bienvenue {user.username} !</p>}
+            {user && user.admin && <p className='fs-5 text-white mx-4'>Vous êtes Administrateur</p>}
             
             <ul className='text-white mx-4 mt-5'>
                 <li>
