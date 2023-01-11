d5a906425a215bf76f051cb59e28a509c2492df9
RAPH <raphaelboutin70@gmail.com>
2023-01-11 11:08:11 +0100

    error category

--- src/Components/CategorieForm.jsx
+++ src/Components/CategorieForm.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
 import * as categorie from "../Services/Category.Service";
+import Error from "./Error";
 import { useNavigate } from "react-router-dom";
 
-
-const CategorieForm = ({ onClick }) => {
-  const submit = () => {
-    onClick();
+  const Input = (props) => {
+    return  <div key={props.index}>
+    <label>Caractéristique {props.index} :</label>
+    <input required className="caracteristiqueValue" type="text"></input>
+    <select className="caracteristiqueValue" name="type">
+      <option value="checkbox">Oui/Non</option>
+      <option value="text">Texte</option>
+      <option value="number">Nombre</option>
+    </select>
+  </div>;
   };
+
+
+const CategorieForm = () => {
+
+
+
+  const [inputList, setInputList] = useState([]);
+  const [toggle, setToggle] = useState(false)
+
 
   const navigate = useNavigate();
 
@@ -17,6 +33,8 @@ const CategorieForm = ({ onClick }) => {
 
   const handleClick = () => {
     setCounter(counter + 1);
+    setInputList(inputList.concat(<Input key={counter} index={counter} />));
+
   };
 
   const createCategorie = () => {
@@ -39,38 +57,26 @@ const CategorieForm = ({ onClick }) => {
     categorie.createCategory(name, caracteristiques);
     navigate("/category");
     }else{
-      alert("Un champs n'est pas remplie")
+      setToggle(true);
     }
-  
+
   };
 
 
   return (
     <div>
+      {toggle && (<Error key={counter} message="Un champs ou plusieurs sont vide" />)}
       <form action="">
       <h2>Création catégorie</h2>
-      <div>
+      <div className="inputDiv">
         <label>Nom : </label>
-        <input required name="categorieName" type="text" onChange={(e) => setName(e.target.value)} ></input>
+        <input required name="categorieName" type="text" onChange={(e) => setName(e.target.value) } ></input>
       </div>
       <div className="separator"></div>
       <h2>Caractéristiques</h2>
 
       <div className="cathegorieInputs">
-        {" "}
-        {Array.from(Array(counter)).map((c, index) => {
-          return (
-            <div key={index}>
-              <label>Caractéristique {index} :</label>
-              <input className="caracteristiqueValue" type="text"></input>
-              <select className="caracteristiqueValue" name="type">
-                <option value="checkbox">Oui/Non</option>
-                <option value="text">Texte</option>
-                <option value="number">Nombre</option>
-              </select>
-            </div>
-          );
-        })}
+      {inputList}
       </div>
       <a className="addThings" onClick={handleClick}>
         + Ajouter une caracteristique{" "}
