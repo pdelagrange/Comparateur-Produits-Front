7c62e1c225077c37ffc6dc4c0b5ef3c506f9cda4
augcolas <augus.colas@orange.fr>
2023-01-11 09:01:32 +0100

    1version

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import './../src/Styles/App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from './Views/Login';
 import Home from './Views/Home';
 import ProductCreation from './Views/ProductCreation';
 import CategorieCreation from './Views/CategorieCreation';
+import Categories from './Views/Categories'
+
 import useToken from "./Utils.jsx/UseToken";
 
 function App() {
@@ -17,6 +20,7 @@ function App() {
           <Route index element={<Home/>} />
           <Route path='/login' element={<Login setToken={setToken}/>}/>
           <Route path='/category/add' element={<CategorieCreation/>}/>
+          <Route path='/category' element={<Categories/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Views/Categories.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header';
+import List from '../Components/CategoriesList';
+
+const Categories = () => {
+    return (
+        <div id='vue'>
+            <Header />
+            <List />
+        </div>
+    )
+}
+
+export default Categories;
