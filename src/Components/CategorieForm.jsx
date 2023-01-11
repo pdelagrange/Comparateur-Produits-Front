6028c9c2bfe5c65a91c0bac28643cc722f3d877e
Pierre-Emmanuel Delagrange <pierre-emmanuel.delagrange@etu.u-bordeaux.fr>
2023-01-11 11:26:22 +0100

    add category

--- src/Components/CategorieForm.jsx
+++ src/Components/CategorieForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import * as categorie from '../Services/Category.Service';
+import * as categorie from "../Services/Category.Service";
 import { useNavigate } from "react-router-dom";
+
 
 const CategorieForm = ({ onClick }) => {
   const submit = () => {
@@ -11,55 +12,74 @@ const CategorieForm = ({ onClick }) => {
 
   const [counter, setCounter] = useState(0);
 
+  const [name, setName] = useState("");
+  
+
   const handleClick = () => {
     setCounter(counter + 1);
   };
 
   const createCategorie = () => {
-    let name = document.querySelector("[name='categorieName']").value;
+    let error = false
+    if(!name.length > 0){ error = true;}
     let caracteristiques = [];
     let inputsCara = document.querySelectorAll("input.caracteristiqueValue");
-    inputsCara.forEach(cara => {
-        caracteristiques.push({
-          name: cara.value
-        })
-    });
-    categorie.createCategory(name,caracteristiques);
-    navigate('/category')
+    let selectCara = document.querySelectorAll("select.caracteristiqueValue");
+    inputsCara.forEach((cara,index) => {
+      console.log(cara.value.length > 0);
+      if(!cara.value.length > 0){error = true;}
+      caracteristiques.push({
+        name: cara.value,
+        type: selectCara[index].value
+      });
 
-    
-  }
+      }
+    );
+    if(!error){
+    categorie.createCategory(name, caracteristiques);
+    navigate("/category");
+    }else{
+      alert("Un champs n'est pas remplie")
+    }
+  
+  };
+
 
   return (
     <div>
+      <form action="">
       <h2>Création catégorie</h2>
-        <label>
-          Nom:
-          <input name="categorieName" type="text" />
-        </label>
+      <div>
+        <label>Nom : </label>
+        <input required name="categorieName" type="text" onChange={(e) => setName(e.target.value)} ></input>
+      </div>
+      <div className="separator"></div>
+      <h2>Caractéristiques</h2>
 
-        <div className="separator"></div>
-        <h2>Caractéristiques</h2>
+      <div className="cathegorieInputs">
+        {" "}
+        {Array.from(Array(counter)).map((c, index) => {
+          return (
+            <div key={index}>
+              <label>Caractéristique {index} :</label>
+              <input className="caracteristiqueValue" type="text"></input>
+              <select className="caracteristiqueValue" name="type">
+                <option value="checkbox">Oui/Non</option>
+                <option value="text">Texte</option>
+                <option value="number">Nombre</option>
+              </select>
+            </div>
+          );
+        })}
+      </div>
+      <a className="addThings" onClick={handleClick}>
+        + Ajouter une caracteristique{" "}
+      </a>
 
-        <div className="cathegorieInputs">
-          {" "}
-          {Array.from(Array(counter)).map((c, index) => {
-            return (
-              <label key={index}>
-                Caractéristique {index} :
-                <input className="caracteristiqueValue" type="text"></input>
-              </label>
-            );
-          })}
-        </div>
-        <a className="addThings" onClick={handleClick}>
-          + Ajouter une caracteristique
-        </a>
-
-        <button type="submit" onClick={createCategorie}>
-          Valider
-        </button>
-
+      <button type="submit" onClick={createCategorie}>
+        Valider
+      </button>
+      </form>
     </div>
   );
 };
