81238672c7b8a26adcf17ba17436b3b5f2b35595
sebastiencandau <scallpie@gmail.com>
2023-01-11 18:16:24 +0100

    right on banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 function Banner(props) {
 
+  const token = useToken();
+  const user = token.getUserConnected();
+
   return (
     
     <div className='d-flex justify-content-between'>
         <h3 className='mx-4 text-white'>
             {props.section}
         </h3>
-        <a href={props.link} className="add-button me-4">{props.buttonText}</a>
+        {user.admin && <a href={props.link} className="add-button me-4">{props.buttonText}</a>}
     </div>
   );
 }
