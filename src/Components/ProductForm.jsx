a21ee60e94ea41fc1e2fd543ddf2f2bcacb17366
Mathis <mathislag@hotmail.fr>
2023-01-11 15:51:02 +0100

    Fin du css de l'ajout de produits

--- src/Components/ProductForm.jsx
+++ src/Components/ProductForm.jsx
@@ -8,7 +8,7 @@ import Error from "./Error";
 
 
 const ProductForm = ({ onClick }) => {
-    
+
     const [options, setOptions] = useState([""]);
     const [selectedFile, setSelectedFile] = useState()
     const [preview, setPreview] = useState()
@@ -16,24 +16,26 @@ const ProductForm = ({ onClick }) => {
     const [name, setName] = useState("");
     const [price, setPrice] = useState(0);
     const [description, setDescription] = useState("");
-    const [category, setCategory] = useState();  
+    const [category, setCategory] = useState();
     const [toggle, setToggle] = useState(false)
+    const [customStyles, setCustomStyles] = useState()
+
 
     const navigate = useNavigate();
 
-    
+
     useEffect(() => {
         const getData = async () => {
-          const arr = [];
-          getCategories().then((response) => response.json()).then((res) => {
-            res.map((category) => {
-              return arr.push({value: category.id, label: category.name});
+            const arr = [];
+            getCategories().then((response) => response.json()).then((res) => {
+                res.map((category) => {
+                    return arr.push({ value: category.id, label: category.name });
+                });
+                setOptions(arr)
             });
-            setOptions(arr)
-          });
         };
         getData();
-      }, []);
+    }, []);
 
 
 
@@ -58,90 +60,97 @@ const ProductForm = ({ onClick }) => {
 
     const onSelectCategory = e => {
         getCategoryCharacteristics(e.value).then((response) => response.json())
-        .then((res) => {
-            setCars(res.characteristic_types);
-            setCategory(res.id);
-        });
+            .then((res) => {
+                setCars(res.characteristic_types);
+                setCategory(res.id);
+            });
     }
 
-    function handleClick (e) {
+    function handleClick(e) {
         e.preventDefault();
         let caracteristiques = [];
         let inputsCara = document.querySelectorAll("input.caracteristiqueValue");
         let empty = false;
         inputsCara.forEach(cara => {
-            if ((cara.value && cara.type != "checkbox") || cara.type == "checkbox"){
-                if (cara.type == "checkbox"){
+            if ((cara.value && cara.type != "checkbox") || cara.type == "checkbox") {
+                if (cara.type == "checkbox") {
                     caracteristiques.push({
                         id: cara.id,
                         value: cara.checked
-                      })
-                }else{
+                    })
+                } else {
                     caracteristiques.push({
                         id: cara.id,
                         value: cara.value
-                      })
+                    })
                 }
-            }else{
+            } else {
                 empty = true;
             }
-    
+
         })
-        if (name != "" && price != 0 && description != "" && category && empty == false){
-            createProduct(name,description,price,category,caracteristiques);
+        if (name != "" && price != 0 && description != "" && category && empty == false) {
+            createProduct(name, description, price, category, caracteristiques);
             navigate('/products');
-        }else{
+        } else {
             setToggle(true);
         }
-      }
 
-    
+
+    }
+
+
     return (
-        <div id="form-wrapper" className='m-4'>
-                <section className="layout">
-                    <div className='m-4'>
-                    {toggle && (<Error message="Un champs ou plusieurs sont vide" />)}
+        <div className='container'>
+            <div className='row '>
+                <div className='col-6 pe-5 border-end border-primary'>
+                    <div className='me-3'>
+                        {toggle && (<Error message="Un champs ou plusieurs sont vide" />)}
                         <span id="elem-wrapper">
-                            <label className='bg-white h2'>Nom :</label> <br/>
-                            <input className='bg-info mt-1 form-control form-rounded w-100' type="text"  onChange={(e) => setName(e.target.value)}/>
-                        </span><br/><br/>
+                            <label className='text-white h2'>Nom :</label> <br />
+                            <input className='bg-info mt-1 text-primary border-0 rounded w-100 inputHeight' type="text" onChange={(e) => setName(e.target.value)} />
+                        </span><br /><br />
                         <span id="elem-wrapper">
-                        <label className='bg-white h2'>Prix :</label><br/>  
-                            <input className='bg-info mt-1 form-control form-rounded w-100' type="number" name="prix" onChange={(e) => setPrice(e.target.value)} placeholder=" €"/>
-                        </span><br/><br/>
+                            <label className='text-white h2'>Prix :</label><br />
+                            <input className='bg-info mt-1 text-primary border-0 rounded w-100 inputHeight' type="number" name="prix" onChange={(e) => setPrice(e.target.value)} placeholder=" €" />
+                        </span><br /><br />
 
                         <span id="elem-wrapper">
-                            <label className='bg-white h2'>Description :</label> <br/>
-                            <textarea className='bg-info mt-1 form-control form-rounded w-100' rows="3" name="description" onChange={(e) => setDescription(e.target.value)}/>
-                        </span><br/><br/>
-                            
+                            <label className='text-white h2'>Description :</label> <br />
+                            <textarea className='bg-info mt-1 text-primary border-0 rounded w-100' rows="3" name="description" onChange={(e) => setDescription(e.target.value)} />
+                        </span><br /><br />
+
                         <span id="elem-wrapper">
-                            <label className='bg-white h2'>Image :</label> <br/>
-                            <input className='bg-info mt-1 form-control form-rounded w-100' type="file" onChange={onSelectFile} name="image" accept="image/png, image/jpeg" />
-                            {selectedFile &&  <img src={preview} /> }
+                            <label className='text-white h2'>Image :</label> <br />
+                            <input className='bg-info mt-1 text-primary border-0 rounded w-100 inputHeight' type="file" onChange={onSelectFile} name="image" accept="image/png, image/jpeg" />
+                            {selectedFile && <img className='rounded imagePreview pt-3' src={preview} />}
                         </span>
                     </div>
-                    <div>
+                </div>
+                <div className='col-6' >
+                    <div className='ms-4 ps-4'>
                         <span id="elem-wrapper">
-                            <label htmlFor="category" className='bg-white h2'>Catégorie  :</label>
-                            <Select placeholder= "Select an individual" options={options} onChange={onSelectCategory} required />
-                                
+                            <label htmlFor="category" className='text-white h2'>Catégorie  :</label>
+                            <div className='pb-4 border-bottom border-primary'>
+                                <Select placeholder="Select an individual" options={options} onChange={onSelectCategory} required />
+                            </div>
                         </span>
-                        <br/>
-                        <div id="caracteristiques">
-                        {cars != null && Array.from(cars).map((c, index) => {
+                        <br />
 
-                            return <div key={index}><label className='bg-white h2'>{c.name} : </label><br/><input className='caracteristiqueValue bg-info mt-1 form-control form-rounded w-100' id={c.id} type={c.type}></input><br/></div> ;
-                        })}
+                        <div id="caracteristiques">
+                            {cars != null && Array.from(cars).map((c, index) => {
+                                return <div key={index}><label className='text-white h2'>{c.name} : </label><br /><input className='caracteristiqueValue bg-info mt-1 text-primary border-0 rounded w-100 inputHeight mb-2' id={c.id} type={c.type}></input><br /></div>;
+                            })}
                         </div>
                         <br/>
-
-                        <button className='bg-white' type="submit" onClick={handleClick}>
-                        Valider
+                        <button className='btn btn-primary mb-3' type="submit" onClick={handleClick}>
+                            Valider
                         </button>
                     </div>
-                </section>
+                </div>
+            </div>
         </div>
+
     );
 }
 export default ProductForm;
