1ffa031f268f185e5ea448a9df0c021032fbd725
augcolas <augus.colas@orange.fr>
2023-01-11 11:00:39 +0100

    categories list

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import useToken from '../Utils.jsx/UseToken';
-import {useNavigate, useNavigation} from "react-router-dom";
+import { useNavigate, useNavigation } from "react-router-dom";
 
 
 function Header() {
 
-  const {token} = useToken();
+  const { token } = useToken();
 
   const navigate = useNavigate();
 
   return (
-    
-<header>
-    <div id='logo'>
+    <header className='mb-3'>
+      <div id='logo'>
         <h1>CardSelector</h1>
-    </div>
-    {!token && <a href="/login">Se connecter</a>}
-    {token
-      &&  <a href="category/add">Ajouter Catégorie</a>
-      &&  <a href="/logout" >Se déconnecter</a>}
-</header>  );
+      </div>
+      {!token && <a href="/login">Se connecter</a>}
+      {token
+        && <a href="category/add">Ajouter Catégorie</a>
+        && <a href="/logout" >Se déconnecter</a>}
+
+    </header>
+  );
 }
 
 export default Header;
