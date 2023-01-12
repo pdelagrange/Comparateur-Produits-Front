0d4d52cae408f35e0bf039cabe42df5798e4febc
Pierre-Emmanuel Delagrange <pierre-emmanuel.delagrange@etu.u-bordeaux.fr>
2023-01-12 10:03:30 +0100

    repair image

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -4,12 +4,21 @@ import {BufferToUri} from "../Utils/Utils";
 
 const Product = (props) => {
 
+    const product = props.product;
+
+    let img;
+    if(!product.image){
+        img = '/icons/no-photo.png';
+    } else {
+        img = BufferToUri(product.image.data);
+    }
+
     return (
         <Card className="m-3 p-3 text-center bg-white text-primary border-0 mb-5">
             <Card.Body className="justify-content-center mb-0" style={{display: "flex"}}>
                 <div>
                     <Card.Title className="product-title fs-1 position-absolute top-0 start-0 p-5 fw-bold">{props.name}</Card.Title>
-                    <Card.Img src={BufferToUri(props.img.data)} className="img-fluid p-1 mt-5"></Card.Img>
+                    <Card.Img src={img} className="img-fluid p-1 mt-5"></Card.Img>
                     <Card className= "bg-primary fs-3 text-center text-black shadow-lg p-1 m-0 fw-bold start-100 bottom-25 w-25 border-secondary">{props.price} €</Card>
                     <Card.Link href="https://google.com" className="position-absolute top-0 end-0 text-secondary">+ Ajouter un objet à comparer</Card.Link>
                 </div>
--- src/Views/ProductDetails.jsx
+++ src/Views/ProductDetails.jsx
@@ -23,7 +23,7 @@ const ProductDetails = () => {
             <Header />
             {
                 product && 
-                <Product name={product.name} price={product.price} img={product.image} />
+                <Product product={product} />
             }
             {   product && 
                 <Details desc={product.description} char={product.characteristic_types} />
